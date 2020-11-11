56dba3866fd9c1351c7780be2dd7b145fcc046b2
code <t.tanit13@gmail.com>
2020-11-11 15:35:24 +0700

    edit web nodejs for web

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express();
 
-router.get('/hello', (req, res) => {
+const PORT = process.env.PORT || 3000
+
+router.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-router.post('/hello', (req, res) => {
+router.post('/', (req, res) => {
   var body = req.body;
   res.send('Hello '+body.name);
 });
+
+router.listen(PORT, () =>{
+	 console.log(`Server is running on port : ${PORT}`)
+})
 
 module.exports = router;
